b78c475d19a266d373ab7b5ade38e1e016a79b56
anonymous-mint <annonymuskmint@gmail.com>
2022-10-28 01:38:41 +0530

    sass with css modules

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/about.module.scss"
+
+const About = () => {
+  return <div className={styles.container}>This is About</div>;
+};
+
+export default About;
---
+++ pages/support.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/support.module.scss";
+
+const Support = () => {
+  return <div className={styles.container}>This is Support</div>;
+};
+
+export default Support;
